2d02a63c790413f427939d9a71ba3f7b0cae0133
Ulysse Rubens <urubens@uliege.be>
2021-10-15 17:23:08 +0200

    Start Zeiss CZI format implementation

---
+++ pims_plugin_format_bioformats/czi.py
@@ -0,0 +1,70 @@
+# * Copyright (c) 2020. Authors: see NOTICE file.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# *      http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+
+from functools import cached_property
+
+from pims.formats import AbstractFormat
+from pims.formats.utils.checker import SignatureChecker
+from pims.formats.utils.engines.vips import VipsHistogramReader
+
+from pims_plugin_format_bioformats.utils.engine import (
+    BioFormatsParser, BioFormatsReader,
+    BioFormatsSpatialConvertor
+)
+
+
+CZI_SIGNATURE = bytearray("ZISRAWFILE".encode("utf8"))
+
+
+class CZIChecker(SignatureChecker):
+    @classmethod
+    def match(cls, pathlike):
+        buf = cls.get_signature(pathlike)
+        return len(buf) > 10 and buf[:10] == CZI_SIGNATURE
+
+
+class CZIFormat(AbstractFormat):
+    """
+    Zeiss CZI format.
+
+    Known limitations:
+    *
+
+    References:
+    * https://github.com/ome/bioformats/blob/develop/components/formats-gpl/src/loci/formats/in/ZeissCZIReader.java
+    * https://docs.openmicroscopy.org/bio-formats/6.7.0/formats/zeiss-czi.html
+    * https://github.com/zeiss-microscopy/libCZI
+
+    """
+    checker_class = CZIChecker
+    parser_class = BioFormatsParser
+    reader_class = BioFormatsReader  # TODO, but useless as conversion always needed ?
+    histogram_reader_class = VipsHistogramReader  # TODO
+    convertor_class = BioFormatsSpatialConvertor
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._enabled = True
+
+    @classmethod
+    def get_name(cls):
+        return "Zeiss CZI"
+
+    @classmethod
+    def is_spatial(cls):
+        return True
+
+    @cached_property
+    def need_conversion(self):
+        return True

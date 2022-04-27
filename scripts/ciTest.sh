152afdb6b6a81cb8cd41723f91f305a575cda9b2
chlmarchal <chloe.marchal@cytomine.com>
2022-04-27 09:53:41 +0200

    create bioformat container

--- scripts/ciTest.sh
+++ scripts/ciTest.sh
@@ -13,7 +13,12 @@ mkdir "$PWD"/ci/test-reports
 touch "$PWD"/ci/test-reports/pytest_unit.xml
 docker build --rm -f scripts/docker/Dockerfile-test --build-arg VERSION_NUMBER=$VERSION_NUMBER -t  cytomine/pims-plugin-format-bioformats-test .
 
+containerIdBioformat=$(docker create --name bioformat -v /data/images:/data/images -e BIOFORMAT_PORT=4321 --restart=unless-stopped cytomine/bioformat:v1.2.0 )
+docker start -ai $containerIdBioformat
+
 containerId=$(docker create --link bioformat:bioformat -v /data/pims:/data/pims -v "$PWD"/ci/test-reports:/app/ci/test-reports -v /tmp/uploaded:/tmp/uploaded -v /tmp/test-pims:/tmp/test-pims cytomine/pims-plugin-format-bioformats-test )
 
 docker start -ai  $containerId
 docker rm $containerId
+docker stop $containerIdBioformat
+docker rm $containeridBioformat

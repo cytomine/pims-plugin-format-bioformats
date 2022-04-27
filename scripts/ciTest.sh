880ee9fa8ec42eb73e5de78ec1e6115ee4777def
chlmarchal <chloe.marchal@cytomine.com>
2022-04-27 10:20:04 +0200

    removed -ai flag

--- scripts/ciTest.sh
+++ scripts/ciTest.sh
@@ -14,7 +14,7 @@ touch "$PWD"/ci/test-reports/pytest_unit.xml
 docker build --rm -f scripts/docker/Dockerfile-test --build-arg VERSION_NUMBER=$VERSION_NUMBER -t  cytomine/pims-plugin-format-bioformats-test .
 
 containerIdBioformat=$(docker create --name bioformat -v /data/images:/data/images -e BIOFORMAT_PORT=4321 --restart=unless-stopped cytomine/bioformat:v1.2.0 )
-docker start -ai $containerIdBioformat
+docker start $containerIdBioformat
 
 containerId=$(docker create --link bioformat:bioformat -v /data/pims:/data/pims -v "$PWD"/ci/test-reports:/app/ci/test-reports -v /tmp/uploaded:/tmp/uploaded -v /tmp/test-pims:/tmp/test-pims cytomine/pims-plugin-format-bioformats-test )
 
